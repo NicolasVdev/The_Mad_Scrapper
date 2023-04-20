01f193806b09571be9851819c0f0adcd3cfb03c9
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-04-20 23:14:16 +0200

    ALT F4

--- spec/Mairie_christmas_spec.rb
+++ spec/Mairie_christmas_spec.rb
@@ -9,8 +9,9 @@ describe "Mairie Christmas" do
 
     it "return the name of town and their mails " do  
      expect(perform).to include {"HERBLAY"}
-     expect(perform).to include {"LA FRETTE SUR SEINE"}
-     expect(perform).to include {"CORMEILLES EN PARISIS"}
+     expect(perform).to include {"LA FRETTE SUR SEINE"=>"mairie@ville-la-frette95.fr"}
+     expect(perform).to include {"CORMEILLES EN PARISIS"=>"contact@ville-cormeilles95.fr"}
+     expect(perform).to include {"mairie@herblay.fr"}
     end 
 
 end
